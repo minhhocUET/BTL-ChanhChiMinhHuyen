311b3eef19e3edf765a86344b33feb3076a1c4bc
ltca <25021622@vnu.edu.vn>
2026-04-03 15:01:10 +0700

    Revert "Son tung mtp"

--- bidding-server/src/main/java/com/uet/bidding/App.java
+++ bidding-server/src/main/java/com/uet/bidding/App.java
@@ -6,7 +6,8 @@ package com.uet.bidding;
  */
 public class App 
 {
-    public static void main( String[] args ){
-        System.out.println( "Hello Huyenvuive!!" )
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }

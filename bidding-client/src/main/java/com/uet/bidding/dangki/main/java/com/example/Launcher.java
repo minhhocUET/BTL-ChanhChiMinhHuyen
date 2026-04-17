efc93344ebc998656a7653d6acf9f5b4627777f3
Huyenvuive123 <daohuyen2007@gmail.com>
2026-04-17 20:39:52 +0700

    cần chuẩn bị csdl (database) để code có thể chạy được tiếp phần đăng nhập, đã thử giả lập đăng nhập (trong hàm checkUserInDatabase trong file LoginController.java)

---
+++ bidding-client/src/main/java/com/uet/bidding/dangki/main/java/com/example/Launcher.java
@@ -0,0 +1,8 @@
+package main.java.com.uet.bidding.dangki.main.java.com.example;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Gọi hàm main của file Main.java
+        Main.main(args);
+    }
+}

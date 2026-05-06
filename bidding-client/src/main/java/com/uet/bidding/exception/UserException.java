ef5eea62346f0826460be73f184d2c9f80daba5d
ltca <25021622@vnu.edu.vn>
2026-05-06 12:11:26 +0700

    thêm ngoại lệ cho user

---
+++ bidding-client/src/main/java/com/uet/bidding/exception/UserException.java
@@ -0,0 +1,12 @@
+package com.uet.bidding.exception;
+
+import java.io.Serializable;
+
+public class UserException extends RuntimeException implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  public UserException(String message) {
+    super(message);
+  }
+}

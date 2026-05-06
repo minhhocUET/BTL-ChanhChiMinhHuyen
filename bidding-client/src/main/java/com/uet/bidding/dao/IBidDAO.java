81506a36690285ffb28d686a5fc025f6f9a6a9de
ltca <25021622@vnu.edu.vn>
2026-05-06 12:09:55 +0700

    thêm interface ứng với các class DAO dễ bảo trì và mở rộng

---
+++ bidding-client/src/main/java/com/uet/bidding/dao/IBidDAO.java
@@ -0,0 +1,13 @@
+package com.uet.bidding.dao;
+
+import com.uet.bidding.model.Bid;
+
+import java.util.List;
+
+public interface IBidDAO {
+  void addBid(Bid bid) throws Exception;
+  List<Bid> getBidsByAuction(int auctionId); // Lấy lịch sử của 1 phiên
+  List<Bid> getBidsByUser(int userId);       // Xem người này đã đấu giá những gì
+  Bid getHighestBid(int auctionId);          // Lấy giá cao nhất hiện tại
+}
+

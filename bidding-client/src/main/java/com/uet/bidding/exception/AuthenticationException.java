24d25786b343f0e4b0122c3096c02c4a5161d548
ltca <25021622@vnu.edu.vn>
2026-05-13 18:00:16 +0700

    chỉnh sửa lại Exception và tạo thêm một số class mới (như ItemException...), làm sạch code

--- bidding-client/src/main/java/com/uet/bidding/exception/AuthenticationException.java
+++ bidding-client/src/main/java/com/uet/bidding/exception/AuthenticationException.java
@@ -1,10 +1,7 @@
 package com.uet.bidding.exception;
 
-import java.io.Serializable;
 
-public class AuthenticationException extends Exception implements Serializable {
-  // ID định danh phiên bản để Client và Server luôn hiểu nhau
-  private static final long serialVersionUID = 1L;
+public class AuthenticationException extends Exception  {
 
   public AuthenticationException(String message) {
     super(message);

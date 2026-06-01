9f127961caca584452fb20d89722cdbafcb5f1e0
ltca <25021622@vnu.edu.vn>
2026-06-01 08:59:05 +0700

    cài các lại dữ liệu kết nối DB thành biến môi trường trên máy riêng

--- bidding-server/src/main/java/com/uet/bidding/dao/DatabaseConnection.java
+++ bidding-server/src/main/java/com/uet/bidding/dao/DatabaseConnection.java
@@ -5,23 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-  // 1. Lưu ý: Hãy đổi chữ 'sys' thành tên database chứa các bảng ERD của bạn (ví dụ: bidding_db)
-  private static final String URL = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/AuctionDB?sslMode=VERIFY_IDENTITY";
-
-  // 2. Giữ nguyên tài khoản và mật khẩu
-  private static final String USER = "PqCPN4Nmo9KQw8B.root";
-  private static final String PASSWORD = "gxryxCrn9Xa7Hysb";
 
   public static Connection getConnection() throws SQLException {
     try {
-      // Đảm bảo đã load Driver (Dành cho các bản MySQL Connector cũ, bản mới có thể bỏ qua)
       Class.forName("com.mysql.cj.jdbc.Driver");
     } catch (ClassNotFoundException e) {
       System.err.println("Không tìm thấy MySQL JDBC Driver!");
       e.printStackTrace();
     }
 
-    // 3. TRUYỀN 3 THAM SỐ VÀO HÀM GET CONNECTION
-    return DriverManager.getConnection(URL, USER, PASSWORD);
+    // Đọc thông tin từ Biến môi trường
+    String url = System.getenv("DB_URL");
+    String user = System.getenv("DB_USER");
+    String password = System.getenv("DB_PASSWORD");
+
+    // Bắt lỗi nếu quên chưa cài đặt biến môi trường
+    if (url == null || user == null || password == null) {
+      System.err.println("❌ THIẾU BIẾN MÔI TRƯỜNG! Vui lòng cấu hình DB_URL, DB_USER, DB_PASSWORD.");
+      throw new SQLException("Thiếu cấu hình biến môi trường cho Database!");
+    }
+
+    return DriverManager.getConnection(url, user, password);
   }
 }
